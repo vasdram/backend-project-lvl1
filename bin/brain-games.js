aa0a8f03f3efdf903ebb5a13b088404c6a38acb2
vas.kozlov <v.kozlov@zaptain.dev>
2021-07-27 17:48:46 +0300

    test action and linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import { greet } from '../src/cli.js'
+import { greet } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-
 console.log(`Hello ${greet()}`);
-
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
 
-
 export const greet = () => {
-    const unswer = readlineSync.question('May I have your name? \n')
-    const name = unswer.length > 0 ? `${unswer[0].toUpperCase()}${unswer.slice(1)}` : 'John DO';
-    
-    return name;
-}
- 
+  const unswer = readlineSync.question('May I have your name? \n');
+  const name = unswer.length > 0 ? `${unswer[0].toUpperCase()}${unswer.slice(1)}` : 'John DO';
+  return name;
+};
