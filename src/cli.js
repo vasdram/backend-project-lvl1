60a17f9c0c8f6c230ab973c45a40cf2146d52586
vas.kozlov <v.kozlov@zaptain.dev>
2021-07-27 16:34:36 +0300

    ch3

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+
+export const greet = () => {
+    const unswer = readlineSync.question('May I have your name? \n')
+    const name = unswer.length > 0 ? `${unswer[0].toUpperCase()}${unswer.slice(1)}` : 'John DO';
+    
+    return name;
+}
+ 
